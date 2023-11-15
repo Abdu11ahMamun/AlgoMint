12c1e471f01ed7adbc85555490a823ec4e97f030
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-11-15 21:58:08 +0600

    Jija templete ready for home and about page

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+posts= [
+    {
+        'author': 'Abdullah',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2023'
+    },
+     {
+        'author': 'Monir',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 27, 2023'
+    },
+     {
+        'author': 'Nasir',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 27, 2023'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/about.html')
 
 
