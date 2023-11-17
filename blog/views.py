2093a993e968f6771c0783c24bf1221e0171fb7d
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-11-18 01:15:19 +0600

    sqlLight added and post fatching feature done

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-
-posts= [
-    {
-        'author': 'Abdullah',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2023'
-    },
-     {
-        'author': 'Monir',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2023'
-    },
-     {
-        'author': 'Nasir',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 27, 2023'
-    }
-]
+# posts= [
+#     {
+#         'author': 'Abdullah',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2023'
+#     },
+#      {
+#         'author': 'Monir',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 27, 2023'
+#     },
+#      {
+#         'author': 'Nasir',
+#         'title': 'Blog Post 3',
+#         'content': 'Third post content',
+#         'date_posted': 'August 27, 2023'
+#     }
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
